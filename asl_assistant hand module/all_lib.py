967b2e6c35cfc721b803abe23a88dcfe10f69435
Kunal <kunalbhalgat2003@gmail.com>
2023-01-21 15:01:45 +0530

    fial project

---
+++ asl_assistant hand module/all_lib.py
@@ -0,0 +1,19 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime  
+import re 
+import wikipedia #pip install wikipedia
+import webbrowser
+import os                            
+import smtplib
+import sys
+import requests
+from socket import timeout
+from bs4 import BeautifulSoup 
+from datetime import date
+import cv2
+import cv2 as cv
+import mediapipe as mp
+import pyttsx3
+import numpy as np
+from googleplaces import GooglePlaces,types,lang
